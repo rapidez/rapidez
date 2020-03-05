3fff4a91ac139037dae520f9c5757903484404b5
Roy Duineveld <royduineveld@gmail.com>
2020-03-05 16:52:33 +0100

    🤫

--- resources/js/components/LayoutFooter.vue
+++ resources/js/components/LayoutFooter.vue
@@ -17,8 +17,6 @@
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+        //
     }
 </script>
--- resources/js/components/LayoutHeader.vue
+++ resources/js/components/LayoutHeader.vue
@@ -14,8 +14,6 @@
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+        //
     }
 </script>
