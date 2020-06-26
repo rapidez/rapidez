5589506664bacfbb7793713323af95ce0cb368fd
Roy Duineveld <royduineveld@gmail.com>
2020-06-26 12:19:34 +0200

    Empty quote bugfix

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -52,7 +52,7 @@ class Quote extends Model
                     ANY_VALUE(quote_address.tax_amount) as tax,
                     ANY_VALUE(quote_address.grand_total) as total
                 ')
-                ->selectRaw('JSON_OBJECTAGG(quote_item.item_id, JSON_OBJECT(
+                ->selectRaw('JSON_REMOVE(JSON_OBJECTAGG(IFNULL(quote_item.item_id, "null__"), JSON_OBJECT(
                     "item_id", quote_item.item_id,
                     "product_id", quote_item.product_id,
                     "sku", quote_item.sku,
@@ -63,7 +63,7 @@ class Quote extends Model
                     "price", quote_item.price_incl_tax,
                     "total", quote_item.row_total_incl_tax,
                     "attributes", quote_item_option.value
-                )) AS items')
+                )), "$.null__") AS items')
                 ->join('quote_id_mask', 'quote_id_mask.quote_id', '=', 'quote.entity_id')
                 ->leftJoin('quote_address', function ($join) {
                     $join->on('quote_address.quote_id', '=', 'quote_id_mask.quote_id')->where('address_type', 'shipping');
@@ -74,7 +74,7 @@ class Quote extends Model
                 ->leftJoin('quote_item_option', function ($join) {
                     $join->on('quote_item.item_id', '=', 'quote_item_option.item_id')->where('code', 'attributes');
                 })
-                ->join('catalog_product_flat_'.config('shop.store').' AS product', 'product.entity_id', '=', 'quote_item.product_id')
+                ->leftJoin('catalog_product_flat_'.config('shop.store').' AS product', 'product.entity_id', '=', 'quote_item.product_id')
                 ->groupBy('quote_id_mask.quote_id');
         });
     }
