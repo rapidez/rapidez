6ccce6c2bd6891842d42f19dc9aca7cb861f9de6
Roy Duineveld <royduineveld@gmail.com>
2020-10-21 14:26:26 +0200

    Turbolinks patch for 404 pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,21 @@ Turbolinks.start()
 import TurbolinksAdapter from 'vue-turbolinks';
 Vue.use(TurbolinksAdapter)
 
+// Monkey patch Turbolinks to render 404 normally
+// See: https://github.com/turbolinks/turbolinks/issues/179
+window.Turbolinks.HttpRequest.prototype.requestLoaded = function() {
+    return this.endRequest(function() {
+        var code = this.xhr.status;
+        if (code >= 200 && code < 300 || code === 404) {
+            var redirectedToLocation = this.xhr.getResponseHeader("Turbolinks-Location");
+            this.delegate.requestCompletedWithResponse(this.xhr.responseText, redirectedToLocation);
+        } else {
+            this.failed = true;
+            this.delegate.requestFailedWithStatusCode(code, this.xhr.responseText);
+        }
+    }.bind(this));
+};
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
