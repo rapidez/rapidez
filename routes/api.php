50ba696f7770496a22a5fef8b727c9c6037e92ad
Roy Duineveld <royduineveld@gmail.com>
2020-11-05 16:40:04 +0100

    Moved the routes to rapidez/core

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use Rapidez\Core\Models\Attribute;
-use Rapidez\Core\Models\OptionSwatch;
 use Illuminate\Http\Request;
-use Rapidez\Core\Models\Quote;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,20 +13,3 @@ use Rapidez\Core\Models\Quote;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('attributes', function () {
-    return Attribute::getCachedWhere(function ($attribute) {
-        return $attribute['filter'] || $attribute['sorting'];
-    });
-});
-
-Route::get('swatches', function () {
-    return OptionSwatch::getCachedSwatchValues();
-});
-
-Route::get('cart/{quoteIdMaskOrCustomerToken}', function ($quoteIdMaskOrCustomerToken) {
-    return Quote::where(function ($query) use ($quoteIdMaskOrCustomerToken) {
-        $query->where('masked_id', $quoteIdMaskOrCustomerToken)
-              ->orWhere('token', $quoteIdMaskOrCustomerToken);
-    })->firstOrFail();
-});
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use Rapidez\Core\Models\Category;
-use Rapidez\Core\Models\Page;
-use Rapidez\Core\Models\Product;
-use Rapidez\Core\Models\Rewrite;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,34 +12,3 @@ use Rapidez\Core\Models\Rewrite;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::view('cart', 'cart.overview');
-Route::view('checkout', 'checkout.overview');
-
-Route::get('{any?}', function ($url = null) {
-    if ($rewrite = Rewrite::firstWhere('request_path', $url)) {
-        if ($rewrite->entity_type == 'category') {
-            if ($category = Category::find($rewrite->entity_id)) {
-                config(['frontend.category' => $category->only('entity_id')]);
-                return view('category.overview', compact('category'));
-            }
-        }
-
-        if ($rewrite->entity_type == 'product') {
-            if ($product = Product::selectForProductPage()->find($rewrite->entity_id)) {
-                $attributes = ['sku', 'super_attributes'];
-                foreach ($product->super_attributes ?: [] as $superAttribute) {
-                    $attributes[] = $superAttribute->code;
-                }
-                config(['frontend.product' => $product->only($attributes)]);
-                return view('product.overview', compact('product'));
-            }
-        }
-    }
-
-    if ($page = Page::firstWhere('identifier', $url ?: 'home')) {
-        return view('page.overview', compact('page'));
-    }
-
-    abort(404);
-})->where('any', '.*');
