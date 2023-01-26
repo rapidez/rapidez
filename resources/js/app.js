be7e8b42916ec574f3e01750a82a97493f4e0bdb
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-01-26 09:52:00 +0100

    Install rapidez/reviews

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
 import "Vendor/rapidez/core/resources/js/app.js";
 import "Vendor/rapidez/account/resources/js/callbacks.js";
 import "Vendor/rapidez/wishlist/resources/js/wishlist.js";
+
+import stars from "Vendor/rapidez/reviews/resources/js/components/Stars.vue";
+Vue.component("stars", stars);
+import starInput from "Vendor/rapidez/reviews/resources/js/components/StarInput.vue";
+Vue.component("star-input", starInput);
