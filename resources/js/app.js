9ec2f51ab661b6a389d626c86ca44650cb994b3d
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-01-27 08:56:21 +0100

    add try catch to wishlist import

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,11 @@ import "Vendor/rapidez/core/resources/js/app.js";
 import "Vendor/rapidez/account/resources/js/callbacks.js";
 import "Vendor/rapidez/wishlist/resources/js/wishlist.js";
 
-import stars from "Vendor/rapidez/reviews/resources/js/components/Stars.vue";
-Vue.component("stars", stars);
-import starInput from "Vendor/rapidez/reviews/resources/js/components/StarInput.vue";
-Vue.component("star-input", starInput);
+try {
+    Vue.component("stars", () =>
+        import("Vendor/rapidez/reviews/resources/js/components/Stars.vue")
+    );
+    Vue.component("star-input", () =>
+        import("Vendor/rapidez/reviews/resources/js/components/StarInput.vue")
+    );
+} catch (e) {}
