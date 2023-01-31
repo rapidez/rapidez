58b27bb711c6ec57201abba6d2188e7192732c08
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-01-31 11:49:25 +0100

    using glob import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,11 @@
 import "Vendor/rapidez/core/resources/js/app.js";
 import "Vendor/rapidez/account/resources/js/callbacks.js";
-import "Vendor/rapidez/wishlist/resources/js/wishlist.js";
+(() => import("Vendor/rapidez/wishlist/resources/js/wishlist.js"))();
 
-try {
-    Vue.component("stars", () =>
-        import("Vendor/rapidez/reviews/resources/js/components/Stars.vue")
-    );
-    Vue.component("star-input", () =>
-        import("Vendor/rapidez/reviews/resources/js/components/StarInput.vue")
-    );
-} catch (e) {}
+const components = import.meta.glob("Vendor/rapidez/reviews/resources/js/components/*.vue", {
+    eager: true,
+});
+
+for (const path in components) {
+    Vue.component(path.split("/").pop().split(".").shift(), components[path].default);
+}
