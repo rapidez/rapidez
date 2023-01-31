21f6cabe7b144a2c3438f3c47d3d1af915f45735
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-01-31 13:09:38 +0100

    import reviews.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,17 +2,4 @@ import "Vendor/rapidez/core/resources/js/app.js";
 
 (() => import("Vendor/rapidez/account/resources/js/callbacks.js"))();
 (() => import("Vendor/rapidez/wishlist/resources/js/wishlist.js"))();
-
-const components = import.meta.glob(
-    "Vendor/rapidez/reviews/resources/js/components/*.vue",
-    {
-        eager: true,
-    }
-);
-
-for (const path in components) {
-    Vue.component(
-        path.split("/").pop().split(".").shift(),
-        components[path].default
-    );
-}
+(() => import("Vendor/rapidez/reviews/resources/js/reviews.js"))();
