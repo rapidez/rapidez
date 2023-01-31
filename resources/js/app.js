6b4e8225712502f148821c577da9f9c25053c2d7
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-01-31 12:05:11 +0100

    using anonymous function for account js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,18 @@
 import "Vendor/rapidez/core/resources/js/app.js";
-import "Vendor/rapidez/account/resources/js/callbacks.js";
+(() => import("Vendor/rapidez/account/resources/js/callbacks.js"))();
+
 (() => import("Vendor/rapidez/wishlist/resources/js/wishlist.js"))();
 
-const components = import.meta.glob("Vendor/rapidez/reviews/resources/js/components/*.vue", {
-    eager: true,
-});
+const components = import.meta.glob(
+    "Vendor/rapidez/reviews/resources/js/components/*.vue",
+    {
+        eager: true,
+    }
+);
 
 for (const path in components) {
-    Vue.component(path.split("/").pop().split(".").shift(), components[path].default);
+    Vue.component(
+        path.split("/").pop().split(".").shift(),
+        components[path].default
+    );
 }
