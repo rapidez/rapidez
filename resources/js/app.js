3116175fe57ada8686f33d39aaa8d3f5a2dfc62a
indy koning <indy@justbetter.nl>
2023-02-21 13:24:50 +0100

    Automatically import all rapidez app.js files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,8 @@
-import 'Vendor/rapidez/core/resources/js/app.js'
-import 'Vendor/rapidez/account/resources/js/callbacks.js'
+import.meta.glob([
+    // Automatically import all installed Rapidez module scripts.
+    'Vendor/rapidez/*/resources/js/app.js',
+    // To exclude a specific file add the path with a !: (https://vitejs.dev/guide/features.html#negative-patterns)
+    // '!Vendor/rapidez/account/resources/js/app.js',
+    // Or to load all js files from another vendor: (https://vitejs.dev/guide/features.html#multiple-patterns)
+    // 'Vendor/<vendor>/*/resources/js/app.js',
+], { eager: true });
