4d701313963b0d80fb5f0842fe97eaf078da80a3
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-06-23 09:05:43 +0200

    Fix frontend for category block

--- resources/views/components/category/block.blade.php
+++ resources/views/components/category/block.blade.php
@@ -1,20 +1,19 @@
 @props(['title', 'description', 'image', 'link', 'button'])
 
-<a
-    class="group relative h-72 w-full overflow-hidden sm:rounded-xl sm:first:row-span-2 sm:first:h-full"
-    href="{{ $link . Rapidez\Core\Models\Config::getCachedByPath('catalog/seo/category_url_suffix', '.html') }}"
->
+<a {{ $attributes->merge([
+    'class' => 'group relative h-72 w-full overflow-hidden sm:rounded-xl sm:first:row-span-2 sm:first:h-full',
+    'href' => $link . Rapidez\Core\Models\Config::getCachedByPath('catalog/seo/category_url_suffix', '.html'),
+]) }}>
     <img
         class="h-full w-full object-cover"
         src="{{ $image }}"
         alt=""
     />
-    <div
-        class="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50 transition group-hover:opacity-40">
+    <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50 transition group-hover:opacity-40">
     </div>
     <div class="absolute bottom-0 left-0 flex flex-col gap-1 p-5 text-white">
         <div>
-            <h2 class="text-lg font-semibold">@lang($title)</h2>
+            <p class="text-lg font-semibold">@lang($title)</p>
             <p class="text-md text-gray-200">@lang($description)</p>
         </div>
         <span>{{ $button }}</span>
