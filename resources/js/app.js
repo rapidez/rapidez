c119158db5b3289c35f467bf14124a288b4828a6
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2024-09-30 10:03:36 +0200

    Automatically import styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,15 @@
 import './components.js'
 
 import.meta.glob([
-    // Automatically import all installed Rapidez module scripts.
+    // Automatically import all installed Rapidez module scripts and styles.
     'Vendor/rapidez/*/resources/js/package.js',
-    // To exclude a specific file add the path with a !: (https://vitejs.dev/guide/features.html#negative-patterns)
+    'Vendor/rapidez/*/resources/css/package.css',
+
+    // Exclude specific JS or CSS files if needed (https://vitejs.dev/guide/features.html#negative-patterns).
     // '!Vendor/rapidez/account/resources/js/package.js',
-    // Or to load all js files from another vendor: (https://vitejs.dev/guide/features.html#multiple-patterns)
+    // '!Vendor/rapidez/account/resources/css/package.css',
+
+    // Or to load all JS or CSS files from another vendor (https://vitejs.dev/guide/features.html#multiple-patterns).
     // 'Vendor/<vendor>/*/resources/js/package.js',
+    // 'Vendor/<vendor>/*/resources/css/package.css',
 ], { eager: true });
