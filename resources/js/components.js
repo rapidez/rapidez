dd1892c639950ba47e0d3051ec6d6459017f0e35
indy koning <indy@justbetter.nl>
2025-11-26 15:47:17 +0100

    Prepare v5 support

--- resources/js/components.js
+++ resources/js/components.js
@@ -1,4 +1,7 @@
-(() => {
+import { defineAsyncComponent } from "vue";
+
+document.addEventListener('vue:loaded', function (event) {
+    const vue = event.detail.vue;
     const components = {
         // Eager load all components not ending with .lazy.vue
         ...import.meta.glob(['./components/**/*.vue', '!./components/**/*.lazy.vue'], { eager: true, import: 'default' }),
@@ -6,13 +9,14 @@
         ...import.meta.glob(['./components/**/*.lazy.vue'], { eager: false, import: 'default' })
     };
     for (const path in components) {
-        let componentName = path
+        const isEager = components[path] !== null && typeof components[path] === "object" && !Array.isArray(components[path])
+        const componentName = path
             .split('/').pop() // Remove directories
             .split('.').shift() // Remove extension
             .replace(/^.|[A-Z]/g, letter => `-${letter.toLowerCase()}`) // PascalCase to snake_case
             .substring(1) // Remove the starting dash
 
         // Register component using their filename.
-        Vue.component(componentName, components[path])
+        vue.component(componentName, isEager ? components[path] : defineAsyncComponent(components[path]))
     }
-})();
+});
