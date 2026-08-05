b9f8b438f03439216bb574b700e344a4139e04e9
Jade Geels <jade@justbetter.nl>
2026-08-05 09:29:59 +0000

    Default schedule for commands (#117)

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('rapidez:index')->hourly()->withoutOverlapping();
+Schedule::command('rapidez:index:update')->everyMinute()->withoutOverlapping();
+
+Schedule::command('cache:clear')->daily();
