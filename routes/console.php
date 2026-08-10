f85f1c82e07c6cc6ad4e5a3d983852f69fd17c62
royduin <royduin@users.noreply.github.com>
2026-08-10 02:38:19 +0000

    Sync with Laravel source

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schedule;
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
 
-Schedule::command('rapidez:index')->hourly()->withoutOverlapping();
-Schedule::command('rapidez:index:update')->everyMinute()->withoutOverlapping();
-
-Schedule::command('cache:clear')->daily();
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
